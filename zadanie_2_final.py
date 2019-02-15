9aabc17189ae2269d5ee8773af5b1e178cd9465f
jamruzanka <m.w.jamrog@gmail.com>
2019-02-15 18:11:11 +0100

    output easier to read

--- zadanie_2_final.py
+++ zadanie_2_final.py
@@ -10,5 +10,6 @@ for file in os.listdir(r"C:\Users\mwjam\Desktop\KURS\zadanie_2\words"):
             data = all_letters.read().lower()
             for char in data:
                 dict[char] +=1
-#print("Number of {}: {}".format(list(dict.keys()), list(dict.values())))
-print(dict)
+print("W słowniku poniżej widać, ile razy każda litera występuje we wszystkich plikach:")
+for key in dict:
+    print([key],"-->",dict[key])
